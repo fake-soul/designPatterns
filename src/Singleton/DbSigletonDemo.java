43aec000e7a03120e66233aaf548f34333df92ae
Bharat <bharat.mulchandani1@olacabs.com>
2021-05-02 14:18:20 +0530

    first commit

--- src/Singleton/DbSigletonDemo.java
+++
@@ -1,8 +0,0 @@
-package Singleton;
-
-public class DbSigletonDemo {
-    public static void main(String[] args) {
-        DbSigleton dbSigleton = DbSigleton.getInstance();
-        System.out.println(dbSigleton);
-    }
-}
